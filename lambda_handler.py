907d31e71a5048741446af256943ad7a5eff52c9
Richard <rick-62@hotmail.co.uk>
2022-01-26 22:22:05 +0000

    failed attempt to fix semlock issues in Lambda

--- lambda_handler.py
+++ lambda_handler.py
@@ -3,16 +3,17 @@ from unittest.mock import patch
 
 def handler(event, context):
     from kedro.framework.project import configure_project
-
-    configure_project("spaceflights_steps_function")
     node_to_run = event["node_name"]
-
+    configure_project("spaceflights_steps_function")
+    
     # Since _multiprocessing.SemLock is not implemented on lambda yet,
     # we mock it out so we could import the session. This has no impact on the correctness
     # of the pipeline, as each Lambda function runs a single Kedro node, hence no need for Lock
     # during import. For more information, please see this StackOverflow discussion:
     # https://stackoverflow.com/questions/34005930/multiprocessing-semlock-is-not-implemented-when-running-on-aws-lambda
     with patch("multiprocessing.Lock"):
+        
+
         from kedro.framework.session import KedroSession
 
         with KedroSession.create(env="aws") as session:
